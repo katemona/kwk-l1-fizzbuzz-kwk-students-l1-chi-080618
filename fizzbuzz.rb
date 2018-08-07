f552c0ddab9099093c95b1199b91b5dc89a19649
katemona <k.monahan@mothermcauley.org>
2018-08-07 20:28:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(number)
- if number % 3 == 0
+  if number % 15 == 0
+    return "FizzBuzz"
+ elsif number % 3 == 0
    return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 15 == 0
-    return "FizzBuzz"
   end
 end
